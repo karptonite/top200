b59cd296b62560f53967cc1107d8a6443d5dbcef
Daniel Karp <danielkarp@gmail.com>
2015-08-27 20:44:30 -0400

    top 200 playlist tools

---
+++ resort.php
@@ -0,0 +1,37 @@
+<?php
+
+$name= $argv[1];
+$year = $argv[2];
+$lastyear = $year - 1;
+
+$fp = fopen( "{$name}top200csv_{$year}.csv", "r" );
+
+while ( $lines[] = fgetcsv( $fp ) )
+{
+}
+
+fclose( $fp );
+
+$fp = fopen( "/Users/karp/Documents/top200/{$name}_{$lastyear}plus.txt", "r" );
+
+$line = fgets( $fp );
+fclose( $fp );
+$blines = explode( "\r", $line );
+
+
+$newlist = $blines[0] . "\r";
+
+$i = 0;
+for ($i=0; $i <= 199; $i++ ) 
+{ 
+	$lastindex = $lines[$i][3];
+	if( is_numeric( $lastindex ) )
+	{
+	$newlist .= $blines[$lastindex] . "\r";
+	}
+ }
+
+$newlistf = fopen( "/Users/karp/Documents/top200/{$name}newtop200_{$year}.txt", "w" );
+
+fwrite( $newlistf, $newlist );
+fclose( $newlistf );
---
+++ shuffle.php
@@ -0,0 +1,38 @@
+<?php
+//Export lists from itunes as plaintext then import the sorted playlists at the end
+$year = date( 'Y' );
+$bill = fopen( "/Users/karp/Documents/top200/billnewtop200_{$year}.txt", "r" );
+
+$line = fgets( $bill );
+$blines = explode( "\r", $line );
+
+fclose( $bill );
+
+$rob = fopen( "/Users/karp/Documents/top200/robnewtop200_{$year}.txt", "r" );
+
+$line = fgets( $rob );
+$rlines = explode( "\r", $line );
+
+fclose( $rob );
+
+$billfirst = $blines[0] . "\r";
+$robfirst  = $blines[0] . "\r";
+
+for ($i=200; $i >= 1; $i-- ) 
+{ 
+	$billfirst .= $blines[$i] . "\r" . $rlines[$i] . "\r";
+	$robfirst  .= $rlines[$i] . "\r" . $blines[$i] . "\r";
+ }
+
+$billfirstf = fopen( "/Users/karp/Documents/top200/billfirsttop200_{$year}.txt", "w" );
+
+fwrite( $billfirstf, $billfirst );
+fclose( $billfirstf );
+
+$robfirstf = fopen( "/Users/karp/Documents/top200/robfirsttop200_{$year}.txt", "w" );
+
+fwrite( $robfirstf, $robfirst );
+fclose( $robfirstf );
+
+
+?>
